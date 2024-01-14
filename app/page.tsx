13902bede4aa1175424a483aa4cf81da843472d8
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2024-01-14 14:14:47 -0600

    unique change made

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-   <h1>Exercise 1.6</h1>
-    )
+    <main className={styles.main}>
+      <h1>1.6 Exercise</h1>
+    </main>
+  );
 }
